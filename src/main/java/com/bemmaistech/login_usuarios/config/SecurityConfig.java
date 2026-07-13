832d8170c530ba9bfe54bbb1556ed28f4e244f21
viniciosChaves <vinicios145@hotmail.com>
2026-07-13 14:03:11 -0300

    fix: ajustes para cors

--- src/main/java/com/bemmaistech/login_usuarios/config/SecurityConfig.java
+++ src/main/java/com/bemmaistech/login_usuarios/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("https://ms-login-usuarios-react.onrender.com"));
+        config.setAllowedOriginPatterns(List.of("https://ms-login-usuarios-react-git-main-bemmastech.vercel.app/"));
         config.setAllowedMethods(List.of("*"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
